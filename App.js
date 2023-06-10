9acfc4b2f57bf996420b99183a1e425175e31258
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-10 17:39:03 +0700

    T6 complete

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -11,15 +11,25 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("HuyHieu");
+  const [session, setSession] = useState({ number: 1, title: "states" });
+  const [current, setCurrent] = useState(true);
+
+  const onclickHandler = () => {
+    setName("update name");
+    setSession({ number: 6, title: "Style" });
+    setCurrent(false);
+  };
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Programming</Text>
-      <Button
-        title="Facebook nguyen huy hieu"
-        onPress={() => {
-          Linking.openURL("https://www.facebook.com/huyhieunguyen19/");
-        }}
-      />
+      <Text>
+        this is section numbert {session.number} + {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? "current session" : "next session"}
+      </Text>
+      <Text style={styles.text}>{name}</Text>
+      <Button title="Update state" onPress={onclickHandler} />
     </View>
   );
 }
