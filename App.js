efbbc3f20049ac3af887d711ce1ab80717b88b0b
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-10 23:15:42 +0700

    [abd]

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.top}></View>
+      <View style={styles.middle}></View>
+      <View style={styles.bottom}></View>
     </View>
   );
 }
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  top: {
+    flex: 0.3,
+    backgroundColor: "black",
+  },
+  middle: {
+    flex: 0.3,
+    backgroundColor: "pink",
+  },
+  bottom: {
+    flex: 0.3,
+    backgroundColor: "#444",
   },
 });
