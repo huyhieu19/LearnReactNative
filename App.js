09f85f29ae6c6876be16b2c6a4b1a3a47ecc3240
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-10 17:11:19 +0700

    T5 create

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Linking,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <Text style={styles.text}>Programming</Text>
+      <Button
+        title="Facebook nguyen huy hieu"
+        onPress={() => {
+          Linking.openURL("https://www.facebook.com/huyhieunguyen19/");
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0ddfdf",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 20,
+    fontStyle: "italic",
+    margin: 10,
   },
 });
